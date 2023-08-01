b8de6446fe4ef1804072f212f0d2a43a7ed71ef5
cryptulien <cryptulien@gmail.com>
2023-08-01 22:49:19 +0200

    Scaling NFT Collections cards, but difficulties to automatize img path line 6 NFTCollections.vue

--- src/views/CommunityWallets.vue
+++ src/views/CommunityWallets.vue
@@ -2,11 +2,12 @@
     <v-main>
         <v-container>
             <v-row>
-                <v-card class="mx-auto" width="250">
+                <v-card class="mx-auto" width="250" v-for="url in urls">
+
                     <v-img src="@/assets/NFTCollections/cyberhuahua_premium.png" height="250px" cover></v-img>
 
-                    <v-card-title>PREMIUM WALLET</v-card-title>
-                    <v-card-title class="premium"></v-card-title>
+                    <v-card-title>{{ url.name }}</v-card-title>
+                    <v-card-title>  </v-card-title>
 
                     <v-card-actions>
                         <v-btn color="orange-lighten-1" variant="text" href="https://www.mintscan.io/chihuahua/account/chihuahua1avfdl66z7ce3v6drr49s84qfdw8jwq0s6jky9v" target="_blank">
@@ -37,58 +38,6 @@
 //        })
 //        .catch((error) => console.error(error));
 //    });
-//
-//    const urls = [
-//    {
-//        name: "Wallet Premium",
-//        url: "https://api.chihuahua.wtf/cosmos/staking/v1beta1/delegations/chihuahua1avfdl66z7ce3v6drr49s84qfdw8jwq0s6jky9v",
-//        selector: "premium",
-//        denom: " $HUAHUA",
-//    },
-//    {
-//        name: "Wallet Zombies",
-//        url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1u0vepvps5h56l9hejccyg9x5aux3m557dl7g77",
-//        selector: ".zombies-text-api-block",
-//        denom: " $TORI",
-//    },
-//    {
-//        name: "Wallet Punk Games Wallet",
-//        url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1g2nuu04v4ckhvknuadudcwz7dpefftzuud9839",
-//        selector: ".punk-games-text-api-block",
-//        denom: " $TORI",
-//    },
-//    {
-//        name: "Wallet Early Punk Games Wallet",
-//        url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1m7lst3e4e3nrgc4ftulryntstt4qx2zztvvmxh",
-//        selector: ".early-punk-games-text-api-block",
-//        denom: " $TORI",
-//    },
-//    {
-//        name: "Wallet Lottery MetaHuahua",
-//        url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1kgmlvpn4dja9zmknms0kpsv5pw3wkvfqcx0fqf",
-//        selector:".lottery-text-api-block",
-//        denom: " $ATOM",
-//    },
-//    {
-//        name: "Wallet Metaverse Improvement",
-//        url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1j3jmy29n4svhrnmqqel4x89p44u55mtzn0nfhs",
-//        selector:".metaverse-text-api-block",
-//        denom: " $ATOM",
-//    },
-//    {
-//        name: "Wallet Marketing & Communication",
-//        url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1adxapj0y5gwlctlfh4f03weuy233l582ljlg26",
-//        selector:".communication-text-api-block",
-//        denom: " $ATOM",
-//    },
-//    {
-//        name: "Wallet PASG Airdrop",
-//        url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1u2fslkjzcrxn3cpu8dnkd2ugy7zalja7erj9ay",
-//        selector:".pasg-text-api-block",
-//        denom: " $ATOM",
-//    },
-//    ];
-//
 export default {
     data: () => ({
       show: false,
@@ -99,7 +48,59 @@ export default {
       show5: false,
       show6: false,
       show7: false,
+      urls:[
+        {
+            name: "PREMIUM WALLET",
+            url: "https://api.chihuahua.wtf/cosmos/staking/v1beta1/delegations/chihuahua1avfdl66z7ce3v6drr49s84qfdw8jwq0s6jky9v",
+            selector: "premium",
+            denom: " $HUAHUA",
+        },
+        {
+            name: "Wallet Zombies",
+            url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1u0vepvps5h56l9hejccyg9x5aux3m557dl7g77",
+            selector: ".zombies-text-api-block",
+            denom: " $TORI",
+        },
+        {
+            name: "Wallet Punk Games Wallet",
+            url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1g2nuu04v4ckhvknuadudcwz7dpefftzuud9839",
+            selector: ".punk-games-text-api-block",
+            denom: " $TORI",
+        },
+        {
+            name: "Wallet Early Punk Games Wallet",
+            url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1m7lst3e4e3nrgc4ftulryntstt4qx2zztvvmxh",
+            selector: ".early-punk-games-text-api-block",
+            denom: " $TORI",
+        },
+        {
+            name: "Wallet Lottery MetaHuahua",
+            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1kgmlvpn4dja9zmknms0kpsv5pw3wkvfqcx0fqf",
+            selector:".lottery-text-api-block",
+            denom: " $ATOM",
+        },
+        {
+            name: "Wallet Metaverse Improvement",
+            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1j3jmy29n4svhrnmqqel4x89p44u55mtzn0nfhs",
+            selector:".metaverse-text-api-block",
+            denom: " $ATOM",
+        },
+        {
+            name: "Wallet Marketing & Communication",
+            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1adxapj0y5gwlctlfh4f03weuy233l582ljlg26",
+            selector:".communication-text-api-block",
+            denom: " $ATOM",
+        },
+        {
+            name: "Wallet PASG Airdrop",
+            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1u2fslkjzcrxn3cpu8dnkd2ugy7zalja7erj9ay",
+            selector:".pasg-text-api-block",
+            denom: " $ATOM",
+        },
+      ]
     }),
+    mounted() {
+        
+    }
   };
 </script>
-
--- src/views/NFTCollections.vue
+++ src/views/NFTCollections.vue
@@ -2,176 +2,24 @@
     <v-main>
         <v-container>
             <v-row>
-                <v-card class="mx-auto" width="250">
-                    <v-img src="@/assets/NFTCollections/cyberhuahua_premium.png" height="250px" cover></v-img>
+                <v-card class="mx-auto" width="250" v-for="title1 in content">
+                    <v-img :src="require(`@/assets/NFTCollections/${title1.image}`)"
+                    height="250px" cover></v-img> 
 
-                    <v-card-title>CYBERHUAHUA</v-card-title>
-                    <v-card-title>PREMIUM</v-card-title>
+                    <v-card-title>{{title1.title1}}</v-card-title>
+                    <v-card-title>{{title1.title2}}</v-card-title>
 
-                    <v-card-subtitle> 49 CYBERHUAHUAS </v-card-subtitle>
+                    <v-card-subtitle> {{title1.amount}} </v-card-subtitle>
 
                     <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://www.stargaze.zone/marketplace/stars1s7uu0v6k83vw9fpfwnc0ugnplhg9uvxn3hvqg8wd678gf7dr38wsglczwg" target="_blank">
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+                        <v-btn color="orange-lighten-1" variant="text" href={{title1.marketplace}} target="_blank">Explore</v-btn>
+                        <v-btn color="orange-lighten-1" variant="text" href={{title1.medium}} target="_blank">Medium</v-btn>
                     </v-card-actions>
 
                     <v-expand-transition>
                         <div v-show="show">
                             <v-divider></v-divider>
-
-                            <v-card-text>
-                                AAA MetaHuahua validator's premium ticket. This is the design of CyberHuahua, a tribute to the blockchain chihuahua and the superhero of Pop Culture.
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
-                </v-card>
-                <v-card class="mx-auto" width="250">
-                    <v-img src="@/assets/NFTCollections/cyberhuahua_zombies.png" height="250px" cover></v-img>
-
-                    <v-card-title>CYBERHUAHUA</v-card-title>
-                    <v-card-title>ZOMBIES</v-card-title>
-
-                    <v-card-subtitle> 200 ZOMBIES </v-card-subtitle>
-
-                    <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://www.stargaze.zone/marketplace/stars198yy57r6sym5tngc2xd9k2pctg9v89q36785zc2cqljukqdglalqzdf2k4" target="_blank">
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show1 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show1 = !show1"></v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show1">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-                                Flesh eating Zombies coming for our Heroes! Will you be the lucky one to mint one of the 20 Rare CyberHuahua Zombies and join the 49 Original CyberHuahuas?
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
-                </v-card>
-                <v-card class="mx-auto" width="250">
-                    <v-img src="@/assets/NFTCollections/toripunks.png" height="250px" cover></v-img>
-
-                    <v-card-title>TORIPUNKS</v-card-title>
-                    <v-card-title>COLLECTION</v-card-title>
-
-                    <v-card-subtitle> 10,000 TORIPUNKS </v-card-subtitle>
-
-                    <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://app.teritori.com/collection/tori-tori1plr28ztj64a47a32lw7tdae8vluzm2lm7nqk364r4ws50rgwyzgsapzezt" target="_blank">
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show2 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show2 = !show2"></v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show2">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-                                #WeAreToriPunks
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
-                </v-card>
-                <v-card class="mx-auto" width="250">
-                    <v-img src="@/assets/NFTCollections/comic_books.png" height="250px" cover></v-img>
-
-                    <v-card-title>COMIC</v-card-title>
-                    <v-card-title>BOOKS</v-card-title>
-
-                    <v-card-subtitle> 3092 COMICS </v-card-subtitle>
-
-                    <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://app.teritori.com/collection/tori-tori1syx6q5rxhntng93pmk9xep25638dqy3dcnppvvytnccxrp7urt7s2570jc" target="_blank">
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show3 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show3 = !show3"></v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show3">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-                                5 exclusive comics for minters, collect all 5 to win a portion of the Early Punks wallet rewards.
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
-                </v-card>
-                
-            </v-row>
-        </v-container>
-        <v-container>
-            <v-row>
-                <v-card class="mx-auto" width="250">
-                    <v-img src="@/assets/NFTCollections/chadpunks.png" height="250px" cover></v-img>
-
-                    <v-card-title>CHADPUNKS</v-card-title>
-                    <v-card-title>COLLECTION</v-card-title>
-
-                    <v-card-subtitle> 1,000 CHADS </v-card-subtitle>
-
-                    <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://app.teritori.com/collection/tori-tori1a8h38nsxu254trpfm0z65mrvguhp544mf9e36670ltm4s89y5wjq7mqevy/mint" target="_blank">
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show4 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show4 = !show4"></v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show4">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-                                The ChadPunks, mysterious men in black, are group of extraordinary individuals within the ranks of the Toripunks community who come and restore faith and balance in the Teritori ecosystem.
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
-                </v-card>
-                <v-card class="mx-auto" width="250">
-                    <v-img src="@/assets/NFTCollections/metahuahua.png" height="250px" cover></v-img>
-
-                    <v-card-title>METAHUAHUA</v-card-title>
-                    <v-card-title>BATCH #1</v-card-title>
-
-                    <v-card-subtitle> 3,333 METAHUAHUAS </v-card-subtitle>
-
-                    <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://market.passage3d.com/mint/juno15yalm0qgg0wzs4etkzzputy7hum8ryzc0rrfzfplnqjeyfrpufwq9u2zwd" target="_blank">
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show5 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show5 = !show5"></v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show5">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-                                The Woofiest passport to the metaverse.
-                            </v-card-text>
+                            <v-card-text>{{title1.description}}</v-card-text>
                         </div>
                     </v-expand-transition>
                 </v-card>
@@ -184,15 +32,63 @@
 
 export default {
     data: () => ({
-      show: false,
-      show1: false,
-      show2: false,
-      show3: false,
-      show4: false,
-      show5: false,
-      show6: false,
-      show7: false,
+      show: true,
+      content: [
+        {
+            title1: "CYBERHUAHUA",
+            title2: "PREMIUM",
+            image: "cyberhuahua_premium.png",
+            amount: "49 CYBERHUAHUAS",
+            marketplace: "https://www.stargaze.zone/marketplace/stars1s7uu0v6k83vw9fpfwnc0ugnplhg9uvxn3hvqg8wd678gf7dr38wsglczwg",
+            medium: "",
+            description: "AAA MetaHuahua validator's premium ticket. This is the design of CyberHuahua, a tribute to the blockchain chihuahua and the superhero of Pop Culture.",
+        },
+        {
+            title1: "CYBERHUAHUA",
+            title2: "ZOMBIES",
+            image: "cyberhuahua_zombies.png",
+            amount: "200 ZOMBIES",
+            marketplace: "https://www.stargaze.zone/marketplace/stars198yy57r6sym5tngc2xd9k2pctg9v89q36785zc2cqljukqdglalqzdf2k4",
+            medium: "",
+            description: "Flesh eating Zombies coming for our Heroes! Will you be the lucky one to mint one of the 20 Rare CyberHuahua Zombies and join the 49 Original CyberHuahuas?",
+        },
+        {
+            title1: "TORIPUNKS",
+            title2: "COLLECTION",
+            image: "",
+            amount: "10,000",
+            marketplace: "https://app.teritori.com/collection/tori-tori1plr28ztj64a47a32lw7tdae8vluzm2lm7nqk364r4ws50rgwyzgsapzezt",
+            medium: "",
+            description: "",
+        },
+        {
+            title1: "COMIC",
+            title2: "BOOKS",
+            image: "",
+            amount: "3,092",
+            marketplace: "https://app.teritori.com/collection/tori-tori1syx6q5rxhntng93pmk9xep25638dqy3dcnppvvytnccxrp7urt7s2570jc",
+            medium: "",
+            description: "5 exclusive comics for minters, collect all 5 to win a portion of the Early Punks wallet rewards.",
+        },
+        {
+            title1: "CHADPUNKS",
+            title2: "COLLECTION",
+            image: "",
+            amount: "1,000",
+            marketplace: "https://app.teritori.com/collection/tori-tori1a8h38nsxu254trpfm0z65mrvguhp544mf9e36670ltm4s89y5wjq7mqevy/mint",
+            medium: "",
+            description: "The ChadPunks, mysterious men in black, are group of extraordinary individuals within the ranks of the Toripunks community who come and restore faith and balance in the Teritori ecosystem.",
+        },
+        {
+            title1: "METAHUAHUA",
+            title2: "BATCH #1",
+            image: "",
+            amount: "3,333",
+            marketplace: "https://market.passage3d.com/mint/juno15yalm0qgg0wzs4etkzzputy7hum8ryzc0rrfzfplnqjeyfrpufwq9u2zwd",
+            medium: "",
+            description: "The Woofiest passport to the metaverse.",
+        },
+      ]
     }),
   }
-
 </script>
