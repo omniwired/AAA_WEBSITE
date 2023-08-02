2b4d70c5f092966d42f93f3c50326ac43e71170a
cryptulien <cryptulien@gmail.com>
2023-08-02 17:35:54 +0200

    Begin Community Wallet page

--- src/views/CommunityWallets.vue
+++ src/views/CommunityWallets.vue
@@ -2,22 +2,25 @@
     <v-main>
         <v-container>
             <v-row>
-                <v-card class="mx-auto" width="250" v-for="url in urls">
+                <v-card class="mx-auto" width="250" v-for="title1 in urls">
+                    <v-img v-bind:src=title1.image
+                    height="250px" cover></v-img> 
 
-                    <v-img src="@/assets/NFTCollections/cyberhuahua_premium.png" height="250px" cover></v-img>
+                    <v-card-title>{{title1.title1}}</v-card-title>
+                    <v-card-title>{{title1.amount}}</v-card-title>
 
-                    <v-card-title>{{ url.name }}</v-card-title>
-                    <v-card-title>  </v-card-title>
+                    <v-card-subtitle> {{title1.denom}} </v-card-subtitle>
 
                     <v-card-actions>
-                        <v-btn color="orange-lighten-1" variant="text" href="https://www.mintscan.io/chihuahua/account/chihuahua1avfdl66z7ce3v6drr49s84qfdw8jwq0s6jky9v" target="_blank">
-                            Link
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+                        <v-btn color="orange-lighten-1" variant="text" v-bind:href=title1.wallet target="_blank">Explore</v-btn>
                     </v-card-actions>
+
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+                            <v-card-text>{{title1.description}}</v-card-text>
+                        </div>
+                    </v-expand-transition>
                 </v-card>
             </v-row>
         </v-container>
@@ -40,67 +43,128 @@
 //    });
 export default {
     data: () => ({
-      show: false,
-      show1: false,
-      show2: false,
-      show3: false,
-      show4: false,
-      show5: false,
-      show6: false,
-      show7: false,
+      show: true,
       urls:[
         {
-            name: "PREMIUM WALLET",
+            title1: "PREMIUM WALLET",
+            amount: "",
             url: "https://api.chihuahua.wtf/cosmos/staking/v1beta1/delegations/chihuahua1avfdl66z7ce3v6drr49s84qfdw8jwq0s6jky9v",
-            selector: "premium",
             denom: " $HUAHUA",
+            image: "",
+            wallet: "",
+            description: "Test",
         },
         {
-            name: "Wallet Zombies",
+            title1: "ZOMBIES WALLET",
+            amount: "",
             url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1u0vepvps5h56l9hejccyg9x5aux3m557dl7g77",
-            selector: ".zombies-text-api-block",
             denom: " $TORI",
+            image: "",
+            wallet: "",
+            description: "",
         },
         {
-            name: "Wallet Punk Games Wallet",
+            title1: "PUNK GAMES",
+            amount: "",
             url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1g2nuu04v4ckhvknuadudcwz7dpefftzuud9839",
-            selector: ".punk-games-text-api-block",
             denom: " $TORI",
+            image: "",
+            wallet: "",
+            description: "",
         },
         {
-            name: "Wallet Early Punk Games Wallet",
+            title1: "EARLY PUNK",
+            amount: "",
             url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1m7lst3e4e3nrgc4ftulryntstt4qx2zztvvmxh",
-            selector: ".early-punk-games-text-api-block",
             denom: " $TORI",
+            image: "",
+            wallet: "",
+            description: "",
         },
         {
-            name: "Wallet Lottery MetaHuahua",
-            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1kgmlvpn4dja9zmknms0kpsv5pw3wkvfqcx0fqf",
-            selector:".lottery-text-api-block",
+            title1: "METAHUAHUA LOTTERY",
+            amount: "",
+            url: "",
             denom: " $ATOM",
+            image: "",
+            wallet: "",
+            description: "",
         },
         {
-            name: "Wallet Metaverse Improvement",
-            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1j3jmy29n4svhrnmqqel4x89p44u55mtzn0nfhs",
-            selector:".metaverse-text-api-block",
+            title1: "METAHUAHUA METAVERSE",
+            amount: "",
+            url: "",
             denom: " $ATOM",
+            image: "",
+            wallet: "",
+            description: "",
         },
         {
-            name: "Wallet Marketing & Communication",
-            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1adxapj0y5gwlctlfh4f03weuy233l582ljlg26",
-            selector:".communication-text-api-block",
+            title1: "METAHUAHUA MARKETING",
+            amount: "",
+            url: "",
             denom: " $ATOM",
+            image: "",
+            wallet: "",
+            description: "",
         },
         {
-            name: "Wallet PASG Airdrop",
-            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1u2fslkjzcrxn3cpu8dnkd2ugy7zalja7erj9ay",
-            selector:".pasg-text-api-block",
+            title1: "PASG AIRDROP",
+            amount: "",
+            url: "",
             denom: " $ATOM",
+            image: "",
+            wallet: "",
+            description: "",
         },
       ]
     }),
     mounted() {
         
     }
+
+//    {
+//            name: "Wallet Zombies",
+//            url: "",
+//            selector: ".zombies-text-api-block",
+//            denom: " $TORI",
+//        },
+//        {
+//            name: "Wallet Punk Games Wallet",
+//            url: "https://rest-teritori.ecostake.com/cosmos/staking/v1beta1/delegations/tori1g2nuu04v4ckhvknuadudcwz7dpefftzuud9839",
+//            selector: ".punk-games-text-api-block",
+//            denom: " $TORI",
+//        },
+//        {
+//            name: "Wallet Early Punk Games Wallet",
+//            url: "",
+//            selector: ".early-punk-games-text-api-block",
+//            denom: " $TORI",
+//        },
+//        {
+//            name: "Wallet Lottery MetaHuahua",
+//            url: "",
+//            selector:".lottery-text-api-block",
+//            denom: " $ATOM",
+//        },
+//        {
+//            name: "Wallet Metaverse Improvement",
+//            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1j3jmy29n4svhrnmqqel4x89p44u55mtzn0nfhs",
+//            selector:".metaverse-text-api-block",
+//            denom: " $ATOM",
+//        },
+//        {
+//            name: "Wallet Marketing & Communication",
+//            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1adxapj0y5gwlctlfh4f03weuy233l582ljlg26",
+//            selector:".communication-text-api-block",
+//            denom: " $ATOM",
+//        },
+//        {
+//            name: "Wallet PASG Airdrop",
+//            url: "https://rest.cosmos.directory/cosmoshub/cosmos/staking/v1beta1/delegations/cosmos1u2fslkjzcrxn3cpu8dnkd2ugy7zalja7erj9ay",
+//            selector:".pasg-text-api-block",
+//            denom: " $ATOM",
+//        },
+
   };
 </script>
